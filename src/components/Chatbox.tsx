889beee9580acbb0bda076b18c5710b52338a64f
Thien Pham <phamanhthien1907@gmail.com>
2024-12-05 22:56:32 +0700

    feat: Implement chatbox UI

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -1,15 +1,37 @@
 import axios from "axios";
-import { addResponseMessage, addUserMessage, Widget } from "react-chat-widget";
+import {
+  addResponseMessage,
+  toggleInputDisabled,
+  Widget,
+  toggleMsgLoader,
+} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import logo from "/logo.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./../styles/custom-chatbox.css"; //
+import { LuMaximize2, LuMinimize2 } from "react-icons/lu";
 
 function Chatbox() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const [reset, setReset] = useState(false);
+  const [showGreetingFirst, setShowGreetingFirst] = useState(true);
+
+  const toggleFullScreen = () => {
+    setIsFullScreen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isOpen && showGreetingFirst) {
+      addResponseMessage("Xin chào, tôi có thể giúp gì cho bạn?");
+      setShowGreetingFirst(false);
+    }
+  }, [isFullScreen, isOpen, showGreetingFirst]);
 
   const handleNewUserMessage = async (newMessage: string) => {
     setReset(!reset);
+    toggleMsgLoader();
+    toggleInputDisabled();
     try {
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -41,6 +63,8 @@ function Chatbox() {
       console.error("Error calling OpenAI API:", error);
       addResponseMessage("Sorry, something went wrong. Please try again.");
     }
+    toggleMsgLoader();
+    toggleInputDisabled();
   };
 
   const handleWidgetToggle = () => {
@@ -48,15 +72,32 @@ function Chatbox() {
   };
 
   return (
-    <Widget
-      handleNewUserMessage={handleNewUserMessage}
-      profileAvatar={logo}
-      title="EnoPath Assistant"
-      subtitle="You can ask anything!"
-      handleToggle={handleWidgetToggle}
-      showCloseButton={true}
-      open={isOpen}
-    />
+    <div
+      className={`chatbox-container ${isFullScreen ? "fullscreen-mode" : ""}`}
+    >
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        title={
+          <div className={`relative ${isFullScreen ? "w-[1300px]" : ""}`}>
+            <div className="">
+              <span>EnoPath Assistant</span>
+            </div>
+            <button
+              onClick={toggleFullScreen}
+              className="text-white hover:scale-110 absolute -right-16 top-[9px]"
+            >
+              {isFullScreen ? <LuMinimize2 /> : <LuMaximize2 />}
+            </button>
+          </div>
+        }
+        subtitle="You can ask anything!"
+        handleToggle={handleWidgetToggle}
+        showCloseButton={true}
+        open={isOpen}
+        className="custom-chatbox"
+      />
+    </div>
   );
 }
 
